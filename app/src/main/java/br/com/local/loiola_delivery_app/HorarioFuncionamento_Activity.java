aa81f07638c80dd593acba6d076d9055ceecdd77
lndsilva <lndsilva@hotmail.com>
2021-05-13 10:58:02 -0300

    card pizzas e imagens

---
+++ app/src/main/java/br/com/local/loiola_delivery_app/HorarioFuncionamento_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.loiola_delivery_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HorarioFuncionamento_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.horario_funcionamento_layout);
+    }
+}
--- app/src/main/java/br/com/local/loiola_delivery_app/Pedidos_Activity.java
+++ app/src/main/java/br/com/local/loiola_delivery_app/Pedidos_Activity.java
@@ -2,13 +2,37 @@ package br.com.local.loiola_delivery_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pedidos_Activity extends AppCompatActivity {
+
+    Button btnFacaPedidos, btnMeusPedidos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedidos_layout);
+
+        btnFacaPedidos = findViewById(R.id.btnfazerpedido);
+        btnMeusPedidos = findViewById(R.id.btnMeusPedidos);
+
+        btnMeusPedidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MeusPedidos_Activity.class));
+            }
+        });
+
+        btnFacaPedidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FacaPedidos_Activity.class));
+            }
+        });
+
+
     }
 }
